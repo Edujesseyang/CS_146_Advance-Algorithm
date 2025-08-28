7dae5852f282917a223711d0cdc6e8da9dc6d20b
Jesse <edujesseyang@gmail.com>
2025-08-28 16:01:05 -0700

    update

---
+++ src/Heap/MyMaxHeap.java
@@ -0,0 +1,24 @@
+package Heap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyMaxHeap {
+    private List<Integer> list;
+
+    public MyMaxHeap(){
+        list = new ArrayList<>();
+    }
+
+    public MyMaxHeap(ArrayList<Integer> input){
+        list = new ArrayList<>();
+        
+    }
+    public int size(){
+        return list.size();
+    }
+
+    private void buildHeap(ArrayList<Integer> inputList){
+
+    }
+}
